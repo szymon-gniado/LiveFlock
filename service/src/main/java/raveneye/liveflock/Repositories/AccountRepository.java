27ce6c1f936770736b6c1a8a4ec3803ae6a83fdd
Szymon Gniado <8l3ndz10r@protonmail.com>
2024-02-23 23:24:59 +0100

    final

--- service/src/main/java/raveneye/liveflock/Repositories/AccountRepository.java
+++ service/src/main/java/raveneye/liveflock/Repositories/AccountRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<raveneye.liveflock.Entities.Account, Long>  {
-    @Query("select * from account_view where username = :username")
-    Optional<raveneye.liveflock.Entities.Account> findAccountViewBy(String username);
+    @Query("select * from account where username like :username")
+    Optional<raveneye.liveflock.Entities.Account> findAccount(String username);
 }
